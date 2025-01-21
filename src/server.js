e6ebbac43368d53a908c646a6ed0a189d7e4c74b
FumoRin <edu.dimasfaizsatria@gmail.com>
2025-01-21 16:26:31 +0700

    ganti port

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use("/api/schedule", scheduleRoutes);
 // Database connection
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
